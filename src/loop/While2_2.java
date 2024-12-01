a0e0ec02bc2a3b699a7a1cc8cd8d7b65fad5c7aa
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2024-12-01 18:51:39 +0900

    2024.12.01 Feat Add Java Loop
    What i studied today
    loop package
    while,do while,break,continue

---
+++ src/loop/While2_2.java
@@ -0,0 +1,20 @@
+package loop;
+
+public class While2_2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 1 ;
+
+        sum = sum + i;
+        i++;
+        System.out.println("i = " + i + " sum = " + sum);
+
+        sum = sum + i;
+        i++;
+        System.out.println("i = " + i + " sum = " + sum);
+
+        sum = sum + i;
+        i++;
+        System.out.println("i = " + i + " sum = " + sum);
+    }
+}
